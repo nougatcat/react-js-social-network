8ff32f830dd5445be9cac4ce06aff49a96d256dc
nougatcat <you@example.com>
2024-04-06 01:07:34 +0300

    Урок 63 б

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+
+
+
+export const getUsers = (currentPage = 1 , pageSize = 5) => {
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+        { withCredentials: true })
+}
