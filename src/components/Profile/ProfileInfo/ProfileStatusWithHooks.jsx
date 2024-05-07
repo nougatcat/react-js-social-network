b785cae9d4695a5738298b4cca9e7a470210bc1d
nougatcat <you@example.com>
2024-05-07 19:11:46 +0300

    Урок 85 useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,11 +1,10 @@
 import css from './ProfileInfo.module.css'
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const ProfileStatusWithHooks = (props) => {
 
-    //let stateWithSetState = useState(false)
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status) //при этом создастся локальная копия status и работать мы будем с ней, не трогая то, что пришло в пропс до тех пор, пока не вызовем updateStatus
 
@@ -19,6 +18,11 @@ const ProfileStatusWithHooks = (props) => {
     const onStatusChange = (event) => {
         setStatus(event.currentTarget.value)
     }
+
+    useEffect (() => { //нужно писать какие-то условия, чтобы useEffect не вызывался бесконечно. Условие прописыватся вторым параметром
+        setStatus(props.status)
+    }, [props.status] ) //эффект для синхронизации статуса глобального с локальным, вместо componentDidUpdate
+
 
     return (
         <div>
