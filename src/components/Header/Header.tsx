1944ec5458a69ba692fe88ce18b6bebfa8b39a45
nougatcat <105752568+nougatcat@users.noreply.github.com>
2024-09-29 02:42:38 +0300

    Logo fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { getIsAuthSelector } from '../../redux/auth-selectors.ts';
 import { AppDispatch } from '../../redux/redux-store';
 import { logout } from "../../redux/auth-reducer.ts";
 import { Button } from 'antd';
+import reactLogo from '../../assets/images/reactLogo.png'
 
 
 const Header: React.FC = () => {
@@ -19,7 +20,7 @@ const Header: React.FC = () => {
 
     return (
         <header className={css.header}>
-            <img className={css.header__image} src='./logo192.png' alt="" />
+            <img className={css.header__image} src={reactLogo} alt="" />
             <div className={css.loginBlock}>
                 {isAuth 
                     ? <Button onClick={_logout}>Выйти</Button>
