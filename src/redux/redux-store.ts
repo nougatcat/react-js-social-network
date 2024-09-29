329f3c18d3bf2efd93b7e4962f043d835f4d2434
nougatcat <105752568+nougatcat@users.noreply.github.com>
2024-09-29 23:48:27 +0300

    Замена legacy_createStore на configureStore из redux toolkit

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,13 +1,12 @@
-import { combineReducers, legacy_createStore as createStore, applyMiddleware, Action, AnyAction } from "redux";
+import { combineReducers, Action } from "redux";
 import profileReducer from "./profile-reducer.ts";
-import dialogsReducer from "./dialogs-reducer.ts";
 import usersReduser from "./users-reducer.ts";
 import authReduser from "./auth-reducer.ts";
-import { ThunkAction, ThunkDispatch, thunk as thunkMiddleware } from "redux-thunk";
+import { ThunkAction, ThunkDispatch} from "redux-thunk";
 import {reducer as formReducer} from 'redux-form';
 import appReduser from "./app-reducer.ts";
-import { composeWithDevTools } from 'redux-devtools-extension';
 import chatReduser from "./chat-reducer.ts";
+import {configureStore} from '@reduxjs/toolkit'
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -21,21 +20,16 @@ let rootReducer = combineReducers({
 type RootReducerType = typeof rootReducer // (globalState: AppStateType) => AppStateType
 export type AppStateType = ReturnType<RootReducerType>
 
-// let state: AppStateType
-
-
 //type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
 // export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
 export type InferActionsTypes<T> = T extends {[key: string]: (...args: any[]) => infer U} ? U : never //укороченная запись вместо закоментированных двух строчек, что выше
 
 export type BaseThunkType<A extends Action = Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A> //по умолчанию может использовать любые экшены
 
-// let store = createStore(rootReducer, 
-//     composeWithDevTools( applyMiddleware(thunkMiddleware))
-//     ); 
-//     //!Если расширение выключено, то использование composeWithDevTools может вызвать ошибку, еще на него почему-то ругается ts
+let store = configureStore({
+    reducer: rootReducer
+})
 
-let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 
 // @ts-ignore
